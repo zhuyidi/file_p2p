c579af56ec1c53971a65e4141c7c637aded15d33
zhuyidi <1311243087@qq.com>
2019-03-07 16:04:27 +0800

    update

--- file_p2p/src/test/java/ClientTest.java
+++ file_p2p/src/test/java/ClientTest.java
@@ -1,9 +1,11 @@
+import client.ClientCenter;
+
 /**
  * by yidi on 3/7/19
  */
 
 public class ClientTest {
     public static void main(String[] args) {
-
+        ClientCenter clientCenter = new ClientCenter();
     }
 }
